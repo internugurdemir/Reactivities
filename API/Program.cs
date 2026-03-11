6c96b7d737ac86a8b8c1db069c6c44d13db42262
Uğur Demir <ardahan1984@hotmail.com>
2026-03-11 18:47:15 +0300

    SignalR_ValueConverter_TimeConverter_P19

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using API.SignalR;
 using Application.Activities.Commands;
 using Application.Activities.Queries;
 using Application.Activities.Validators;
@@ -30,6 +31,7 @@ builder.Services.AddDbContext<AppDbContext>(
     }
 );
 builder.Services.AddCors();
+builder.Services.AddSignalR();
 builder.Services.AddMediatR
             (x =>
             {
@@ -82,6 +84,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapGroup("api").MapIdentityApi<User>();
+app.MapHub<CommentHub>("/comments");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
